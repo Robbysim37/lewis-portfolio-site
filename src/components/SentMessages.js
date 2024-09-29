6cce5f8beb69cc0d5ee9897ffbeeff132c8475dd
Robert Lewis <Robbysim37@gmail.com>
2024-09-28 20:57:08 -0400

    fixed layout

--- src/components/SentMessages.js
+++ src/components/SentMessages.js
@@ -10,50 +10,31 @@ export default function SentMessages({timeStamp}) {
         brt-rounded-3xl text-[2vw] text-gray-300'>Robert Lewis</div>
 
         <div className='w-full h-[93%] flex flex-col-reverse'>
-            <Message sender="user" timeStamp={timeStamp}/>
-            <Message sender="computer" timeStamp={timeStamp}/>
+            <Message sender="user" timeStamp={timeStamp}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Message>
+            <Message sender="computer" timeStamp={timeStamp}>test</Message>
         </div>
 
   </div>
   )
 }
 
-function Message({sender,timeStamp}) {
+function Message({sender,timeStamp,children}) {
     return(
-        <div className=' flex flex-col'>
-            {sender === "user" && 
-                <div className='w-[75%] mb-10 mr-2 flex flex-col justify-end items-end self-end'>
-                {/* Message Time Stamp */}
-                    <div className='mr-[7%] text-gray-500'>You {timeStamp}</div>
-                  {/* Text avatar wrapper */}
-                    <div className='flex'>
-                        {/* Text */}
-                        <div className='bg-green-700 text-white max-w-[95%] rounded-3xl p-2 text-start'>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
-                            labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
-                            ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
-                            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-                            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt 
-                            mollit anim id est laborum.
-                        </div>
-                        {/* User Picture */}
-                        <RxAvatar className='text-green-500 ml-1 w-[5%] h-[40%]'/>
-                    </div>
-                </div>}
-              {sender === "computer" && 
-                <div className='w-[75%] mb-5 ml-2 flex flex-col justify-start items-start self-start'>
-                    <div className="ml-[9%] text-gray-500"> Robert Lewis {timeStamp}</div>
-                    <div className='flex'>
-                        {/* ARR image */}
-                        <div className='h-14 w-14 overflow-hidden'>
-                            <img className='h-full w-full rounded-[100%] object-cover mr-1' alt="img" src={ARRImage}></img>
-                        </div>
-                        {/* Fake Text */}
-                        <div className='bg-green-700 text-white w-full self-start rounded-3xl p-2 text-start'>
-                            Test string
-                        </div>
-                    </div>
-              </div>}
+        <>
+        <div className={sender === "computer" ? "full-message-wrapper" : "full-message-wrapper-user"}>
+
+            {sender === "computer" && <div className='image-container'><img className='robby-image' src={ARRImage} /></div>}
+            
+            <div className='message-and-timestamp'>
+
+                {sender === "computer" && <div className='timestamp'>Robert Lewis {timeStamp}</div>}
+                {sender === "user" && <div className='timestamp-user'>You {timeStamp}</div>}
+
+                <div className='message'>{children}</div>
+
+            </div>
+            {sender === "user" && <div className='image-container'><RxAvatar className="user-image"size={48}/></div>}
         </div>
+        </>
     )
 }
