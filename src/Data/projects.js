0763f3ce704cb72abaff2771192e46e25173bee8
Robby Lewis <Robbysim37@gmail.com>
2024-09-30 14:29:54 -0400

    porting over real data

---
+++ src/Data/projects.js
@@ -0,0 +1,40 @@
+import binarySudoku from "../assets/projectImages/binary.png"
+import connectFour from "../assets/projectImages/connectFour.png"
+import knuckleBones from "../assets/projectImages/knuckleBones.png"
+import MGST from "../assets/projectImages/MGST.png"
+import boxDefender from "../assets/projectImages/boxDefender.png"
+
+const projectsArr = [
+    {
+        title: "Box Defender Excel",
+        imgURL: boxDefender,
+        projectLink: "",
+        description: "Placeholder"
+    },
+    {
+        title: "Binary Sudoku Solver",
+        imgURL: binarySudoku,
+        projectLink: "https://binary-sudoku-solver.vercel.app/",
+        description: "Placeholder"
+    },
+    {
+        title: "Angular Knucklebones",
+        imgURL: knuckleBones,
+        projectLink: "https://angular-knucklebones-demo.vercel.app/",
+        description: "Placeholder"
+    },
+    {
+        title: "Two Player Connect Four",
+        imgURL: connectFour,
+        projectLink: "https://connect-four-react-zeta.vercel.app/",
+        description: "Placeholder"
+    },
+    {
+        title: "Maple Grove Student Tracker",
+        imgURL: MGST,
+        projectLink: "https://youtu.be/sIBnrap4mIs",
+        description: "Placeholder"
+    },
+]
+
+export default projectsArr
--- src/components/Projects/ProjectsPage.js
+++ src/components/Projects/ProjectsPage.js
@@ -1,42 +1,47 @@
 import React from "react";
+import { useState } from "react";
+import projectsArr from "../../Data/projects";
 
 const ProjectsPage = () => {
+
+    const [projects] = useState(projectsArr)
+    
+    const jsxArr = []
+    const cardRows = Math.ceil(projectsArr.length / 3)
+    let currCardNumber = -1
+
+    const addCards = () => {
+        currCardNumber++
+        return(
+            <ProjectCard></ProjectCard>
+        )
+    }
+
+    for(let i = 0; i < cardRows; i++){
+        jsxArr.push(<CardRow>
+            {projects[currCardNumber + 1] && addCards()}
+            {projects[currCardNumber + 1] && addCards()}
+            {projects[currCardNumber + 1] && addCards()}
+        </CardRow>)
+    }
+
     return(
         <div className="bg-gray-700 h-[96%] w-[98%] rounded-3xl flex flex-col overflow-scroll no-scrollbar">
             <div className="w-full h-[7%] brt-3xl bg-gray-400">Test</div>
-            <CardRow>
-                <ProjectCard></ProjectCard>
-                <ProjectCard></ProjectCard>
-                <ProjectCard></ProjectCard>
-            </CardRow>
-            <CardRow>
-                <ProjectCard></ProjectCard>
-                <ProjectCard></ProjectCard>
-                <ProjectCard></ProjectCard>
-            </CardRow>
-            <CardRow>
-                <ProjectCard></ProjectCard>
-                <ProjectCard></ProjectCard>
-                <ProjectCard></ProjectCard>
-            </CardRow>
-            <CardRow>
-                <ProjectCard></ProjectCard>
-                <ProjectCard></ProjectCard>
-                <ProjectCard></ProjectCard>
-            </CardRow>
+                {jsxArr}
         </div>
     )
 }  
 
 const CardRow = ({children}) => {
     return(
-        <div className=" w-full h-fit flex justify-evenly">{children}</div>
+        <div className=" w-full h-fit flex justify-start">{children}</div>
     )
 }
 
 const ProjectCard = () => {
     return(
-        <div className="flex flex-col w-[30%] h-fit m-4 group">
+        <div className="flex flex-col w-[30%] h-fit mx-[1.66%] mt-8 group">
             <div className="h-[25vh] bg-white"></div>
             <div className="scale-0 h-0 bg-blue-500 group-hover:scale-100 group-hover:h-12 transition-all duration-200">
                 test this text out but the length of the text is going to effect this component
