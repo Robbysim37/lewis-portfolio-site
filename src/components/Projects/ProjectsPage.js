f35d90dbb04f68bec9b65ddf2acb632aafa5a704
Robert Lewis <Robbysim37@gmail.com>
2024-09-30 20:33:10 -0400

    projects page pretty much complete

--- src/components/Projects/ProjectsPage.js
+++ src/components/Projects/ProjectsPage.js
@@ -12,8 +12,10 @@ const ProjectsPage = () => {
 
     const addCards = () => {
         currCardNumber++
+        const currProject = projects[currCardNumber]
+        console.log(currProject.imgURL)
         return(
-            <ProjectCard></ProjectCard>
+            <ProjectCard title={currProject.title} imgURL={currProject.imgURL}>{currProject.description}</ProjectCard>
         )
     }
 
@@ -26,8 +28,9 @@ const ProjectsPage = () => {
     }
 
     return(
-        <div className="bg-gray-700 h-[96%] w-[98%] rounded-3xl flex flex-col overflow-scroll no-scrollbar">
-            <div className="w-full h-[7%] brt-3xl bg-gray-400">Test</div>
+        <div className="bg-gray-800 h-[96%] w-[98%] rounded-3xl flex flex-col overflow-scroll no-scrollbar">
+            <div className="w-full h-[7%] brt-3xl bg-gray-700 text-white text-[2vw] 
+            flex justify-center items-center">Projects</div>
                 {jsxArr}
         </div>
     )
@@ -39,13 +42,19 @@ const CardRow = ({children}) => {
     )
 }
 
-const ProjectCard = () => {
+const ProjectCard = ({children,imgURL,title}) => {
     return(
         <div className="flex flex-col w-[30%] h-fit mx-[1.66%] mt-8 group">
-            <div className="h-[25vh] bg-white"></div>
-            <div className="scale-0 h-0 bg-blue-500 group-hover:scale-100 group-hover:h-12 transition-all duration-200">
-                test this text out but the length of the text is going to effect this component
-                this really sucks man
+                <div className=" bg-gray-700 text-gray-300 h-0 overflow-hidden brt-rounded-3xl
+                 flex justify-center items-center text-lg group-hover:h-12 
+                 group-hover:scale-100 transition-all duration-700">{title}</div>
+                <div className="h-[30vh]">
+                    <img className="w-full h-full object-fit rounded-3xl 
+                    group-hover:rounded-none transition-all duration-1000" src={imgURL} alt="test"></img>
+                </div>
+            <div className=" overflow-hidden max-h-0 bg-gray-700 text-gray-300 brb-rounded-3xl 
+             group-hover:max-h-96 group-hover:p-4 transition-all duration-700">
+                {children}
             </div>
         </div>
     )
